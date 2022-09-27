cdeedffed74270acc7609ab9dab35d46f00910a1
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2022-09-27 15:33:40 +0530

    Add files via upload

---
+++ 02.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    //printing first ten natural numbers
+     printf("First ten natural numbers.\n");
+    for(int i=1;i<11;i++)
+        printf(" %d,",i);
+    return 0;
+}
