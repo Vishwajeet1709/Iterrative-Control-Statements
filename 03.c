0ab61b6eb730da665ae8298a500a8c7150a6a28e
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2022-09-27 15:34:15 +0530

    Add files via upload

---
+++ 03.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    //first ten natural numbers in revers order
+    printf("First ten natural numbers in revers order.\n");
+    for(int i=0;i<10;i++)
+        printf(" %d,",10-i);
+    return 0;
+}
